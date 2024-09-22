6333edaf7ca6ece53f6f19b678e7404d29955f76
Yogesh Jha <nityanandrekhajha@gmail.com>
2024-09-22 23:01:48 +0530

    Added React using Vite folder and code

---
+++ ES6 Concept Basic/script.js
@@ -0,0 +1,27 @@
+let getListOfProductsElements = document.querySelector(".list-of-products");
+
+function renderProducts(getProducts) {
+  getListOfProductsElements.innerHTML = getProducts
+    .map((singleProductItem) => `<p>${singleProductItem.title}</p>`)
+    .join(" ");
+}
+
+async function fetchListOFProducts() {
+  try {
+    const apiResponse = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+    });
+
+    const result = await apiResponse.json();
+
+    if (result.products.length > 0) {
+      renderProducts(result.products);
+    }
+
+    console.log(result);
+  } catch (e) {
+    console.log("The Error is ", e);
+  }
+}
+
+fetchListOFProducts();
